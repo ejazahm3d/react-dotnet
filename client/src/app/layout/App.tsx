ca2e4e2f2ac32821a8356fe00bad67242220ec83
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2019-10-03 14:41:26 +0500

    Added axios api

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 
 import { IEvent } from "../models/event";
 import NavBar from "../../features/nav/NavBar";
 import Container from "../../features/container/Container";
 import EventDashboard from "../../features/events/dashboard/EventDashboard";
+import agent from "../api/agent";
 
 const App: React.FC = () => {
     const [events, setEvents] = useState<IEvent[]>([]);
@@ -12,11 +12,7 @@ const App: React.FC = () => {
     const [editMode, setEditMode] = useState(false);
 
     useEffect(() => {
-        axios
-            .get<IEvent[]>("http://localhost:5000/api/events")
-            .then(response => {
-                setEvents(response.data);
-            });
+        agent.Events.list().then(res => setEvents(res));
     }, []);
 
     const handleSelectedEvent = (id: String): void => {
